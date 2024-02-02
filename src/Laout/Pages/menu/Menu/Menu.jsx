cd930c3098e549ead0c6488a2b57946068f39bf0
Kamrul Hassan <kamrulthedev@gmail.com>
2024-02-02 12:05:04 +0600

    Catagory-commit

--- src/Laout/Pages/menu/Menu/Menu.jsx
+++ src/Laout/Pages/menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Home/Saherd/Cover/Cover';
 import imges from './../../../../assets/menu/banner3.jpg'
-import PopulerMeno from '../../Home/PoplarMeno/PopulerMeno';
+
 
 
 const Menu = () => {
@@ -14,36 +14,6 @@ const Menu = () => {
                 <Cover img={imges}
                     title={'OUR MENU'}
                 ></Cover>
-
-                <div className='-mt-16'>
-                        <PopulerMeno></PopulerMeno>
-                    </div>
-                <div className='mt-14'>
-                    <Cover img={imges}
-                        title={'OUR MENU'}
-                    ></Cover>
-                    <div className='-mt-16'>
-                        <PopulerMeno></PopulerMeno>
-                    </div>
-                </div>
-
-                <div className='mt-14'>
-                    <Cover img={imges}
-                        title={'OUR MENU'}
-                    ></Cover>
-                    <div className='-mt-16'>
-                        <PopulerMeno></PopulerMeno>
-                    </div>
-                </div>
-                
-                <div className='mt-14'>
-                    <Cover img={imges}
-                        title={'OUR MENU'}
-                    ></Cover>
-                    <div className='-mt-16'>
-                        <PopulerMeno></PopulerMeno>
-                    </div>
-                </div>
             </div>
         </div>
     );
