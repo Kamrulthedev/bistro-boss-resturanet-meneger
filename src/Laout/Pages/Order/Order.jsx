1a67a356dc05bea4f131687d7ac1041b69a0caf6
Kamrul Hassan <kamrulthedev@gmail.com>
2024-02-03 02:21:20 +0600

    order-commit

--- src/Laout/Pages/Order/Order.jsx
+++ src/Laout/Pages/Order/Order.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import Cover from "../Home/Saherd/Cover/Cover";
+import img3 from './../../../assets/shop/banner2.jpg';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+
 
 const Order = () => {
+    const [tabIndex, setTebIndex] = useState();
     return (
         <div>
-            
+            <Cover img={img3} title='OUR SHOP' ></Cover>
+
+            <Tabs className='text-center mt-14 mb-8' defaultIndex={tabIndex} onSelect={(index) => setTebIndex(index)}>
+                <TabList>
+                    <Tab>SALAD</Tab>
+                    <Tab>PIZZA</Tab>
+                    <Tab>SOUPS</Tab>
+                    <Tab>DESSETS</Tab>
+                    <Tab>DRINKS</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
 
-export default Order;
+export default Order; 
--- src/Laout/Roots.jsx
+++ src/Laout/Roots.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLout from "./MainLout";
 import Home from "./Pages/Home/Home/Home";
 import Menu from "./Pages/menu/Menu/Menu";
+import Order from "./Pages/Order/Order";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path:'/menu',
                 element:<Menu></Menu>
+            },
+            {
+                path:'order',
+                element:<Order></Order>
             }
         ]
     }
